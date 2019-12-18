1822c6ba5b9cd9791c565e449842fd4dd4c43018
fostyak-andriy <56426629+fostyak-andriy@users.noreply.github.com>
2019-12-18 19:52:04 +0200

    Delete laba1 Reversed.py

--- laba1 Reversed.py
+++
@@ -1,9 +0,0 @@
-def reverse(string):
-    reversed_string = ""
-    for i in string:
-        reversed_string = i + reversed_string
-    print(reversed_string)
-
-
-string = input("Enter a string: ")
-reverse(string)
